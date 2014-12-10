322c46a569279aa1fe6152cc562863066f30bd64
Jason Damiani <jasondamiani@gmail.com>
2014-12-10 01:43:21 -0500

    OLS on turnstile weather data

--- project_3/other_linear_regressions/advanced_linear_regressions.py
+++ project_3/other_linear_regressions/advanced_linear_regressions.py
@@ -2,39 +2,43 @@ import numpy as np
 import pandas as pd
 import scipy
 import statsmodels
+from datetime import datetime
 
-"""
-In this optional exercise, you should complete the function called 
-predictions(turnstile_weather). This function takes in our pandas 
-turnstile weather dataframe, and returns a set of predicted ridership values,
-based on the other information in the dataframe.  
-
-You should attempt to implement another type of linear regression, 
-that you may have read about, such as ordinary least squares regression: 
-http://en.wikipedia.org/wiki/Ordinary_least_squares
-
-This is your playground. Go wild!
-
-How does your choice of linear regression compare to linear regression
-with gradient descent?
-
-You can look at the information contained in the turnstile_weather dataframe below:
-https://www.dropbox.com/s/meyki2wl9xfa7yk/turnstile_data_master_with_weather.csv
-
-Note: due to the memory and CPU limitation of our amazon EC2 instance, we will
-give you a random subset (~15%) of the data contained in turnstile_data_master_with_weather.csv
-
-If you receive a "server has encountered an error" message, that means you are hitting 
-the 30 second limit that's placed on running your program. See if you can optimize your code so it
-runs faster.
-"""
 
 def predictions(weather_turnstile):
-    #
-    # Your implementation goes here. Feel free to write additional
-    # helper functions
-    # 
+
+    f = lambda x: datetime.strptime(x, "%Y-%m-%d").weekday()
+
+    # Select Features (try different features!)
+    features = weather_turnstile[['rain', 'fog', 'precipi', 'Hour', 'mintempi', 'maxtempi']]
+    features['weekday'] = weather_turnstile['DATEn'].apply(f)
+
+    # Add UNIT to features using dummy variables
+    dummy_units = pd.get_dummies(weather_turnstile['UNIT'], prefix='unit')
+    features = features.join(dummy_units)
+    
+    # Create polynomial features
+    #features['precipi'] = features['precipi']**3
+    
+    # Values
+    values = weather_turnstile[['ENTRIESn_hourly']]
+    m = len(values)
+
+    features['ones'] = np.ones(m) # Add a column of 1s (y intercept)
+    
+    #print features.tail()
+    
+    # Convert features and values to numpy arrays
+    features_array = np.array(features)
+    values_array = np.array(values).flatten()
+    features_array_transpose = features_array.transpose()
+    
+    theta = np.dot(np.dot(np.linalg.pinv(np.dot(features_array_transpose, features_array)), features_array_transpose), values_array)
+    prediction = np.dot(features_array, theta)
+
     return prediction
+
+
 
 def compute_r_squared(data, predictions):
     SST = ((data-np.mean(data))**2).sum()
@@ -45,7 +49,7 @@ def compute_r_squared(data, predictions):
 
 if __name__ == "__main__":
     input_filename = "turnstile_data_master_with_weather.csv"
-    turnstile_master = pd.read_csv(input_filename)
+    turnstile_master = pd.read_csv("turnstile_data_master_with_weather.csv")
     predicted_values = predictions(turnstile_master)
     r_squared = compute_r_squared(turnstile_master['ENTRIESn_hourly'], predicted_values) 
 
